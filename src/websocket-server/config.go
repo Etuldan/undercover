3533bc2ec525ddac7f3dfe33d722c5cf04771fc9
Arnaud RENAUD-GOUD <arnaud@renaud-goud.fr>
2023-12-20 17:33:29 +0100

    config update

--- src/websocket-server/config.go
+++ src/websocket-server/config.go
@@ -10,4 +10,9 @@ type Config struct {
 		Url    string `yaml:"url"`
 		ApiKey string `yaml:"apikey"`
 	} `yaml:"seq"`
+	Game struct {
+		nbUndercover int `yaml:"nbUndercover"`
+		NbWhite      int `yaml:"nbWhite"`
+		NbTurn       int `yaml:"nbTurn"`
+	} `yaml:"game"`
 }
