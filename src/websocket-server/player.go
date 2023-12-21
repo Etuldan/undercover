02e6a2e4fa2596d90e1ec4073e3c734f366496fe
Arnaud RENAUD-GOUD <arnaud@renaud-goud.fr>
2023-12-21 16:30:16 +0100

    new eliminated property

--- src/websocket-server/player.go
+++ src/websocket-server/player.go
@@ -8,11 +8,12 @@ const (
 )
 
 type Player struct {
-	Nickname string  `json:"nickname"`
-	Rank     Rank    `json:"rank"`
-	Role     Role    `json:"-"`
-	Client   *Client `json:"-"`
-	Position int     `json:"position"`
+	Nickname   string  `json:"nickname"`
+	Rank       Rank    `json:"rank"`
+	Role       Role    `json:"-"`
+	Client     *Client `json:"-"`
+	Position   int     `json:"position"`
+	Eliminated bool    `json:"eliminated"`
 }
 
 type Role int
@@ -25,9 +26,10 @@ const (
 
 func newPlayer(nickname string, client *Client) *Player {
 	return &Player{
-		Nickname: nickname,
-		Rank:     Guest,
-		Client:   client,
-		Role:     Civilian,
+		Nickname:   nickname,
+		Rank:       Guest,
+		Client:     client,
+		Role:       Civilian,
+		Eliminated: false,
 	}
 }
